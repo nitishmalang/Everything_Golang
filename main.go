5fb065b8fe50bfb1bffa809fa12b32fc37855509
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2024-11-03 22:41:47 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,24 +1,3 @@
-package main
-
-import "fmt"
-
-func main() {
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
-	}
-}	*/
-
-package main
-
-import "fmt"
-
-func main() { 
-	i := 0
-	for i < 5  {
-		fmt.Println(i)
-		i++
-	}
-} */
 
 
 package main
