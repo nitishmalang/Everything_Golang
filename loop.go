e5934aea64aef05345b7448315cc79aa5d187aa5
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2024-11-03 22:42:19 +0530

    Create loop.go

---
+++ loop.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
+}	
+package main
+
+import "fmt"
+
+func main() { 
+	i := 0
+	for i < 5  {
+		fmt.Println(i)
+		i++
+	}
+} 
