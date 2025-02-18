ba750053940131d238e637a8d9b4cc714b7d527c
Nitish Malang <71919457+nitishmalang@users.noreply.github.com>
2025-02-18 15:46:08 +0530

    Update defer.go

--- defer.go
+++ defer.go
@@ -7,3 +7,17 @@ func main() {
 
 	fmt.Println("hello")
 }
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
+}
+
